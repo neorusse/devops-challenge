e4b21abd4481351a483b39ffee7c357947d7c4cd
Russell Nyorere <neorusse@gmail.com>
2020-10-20 17:57:18 +0100

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <img className="header-logo" src="./logo.png" alt="logo" />
+      <img className="header-barcode" src="./bar-code.jpeg" alt="barcode" />
+      <div></div>
+    </div>
+  );
+}
+
+export default Header;
