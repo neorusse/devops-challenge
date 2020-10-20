49ed91edfbcc89ae916a4acf378092d4ae1c6da2
Russell Nyorere <neorusse@gmail.com>
2020-10-20 17:58:25 +0100

    Compose Intro component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import Intro from "./components/Intro";
 import Form from "./components/Form";
 import "./App.css";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <Intro />
       <Form />
     </div>
   );
